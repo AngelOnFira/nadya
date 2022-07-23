6a435ecc886a0eadc2ab089ce7aa388c52da401d
Forest Anderson <forestkzanderson@gmail.com>
2022-07-23 13:32:16 -0400

    Exit to entrypoint working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 fn main() {
     // Load the file in tests/hello_world
@@ -15,7 +18,9 @@ fn main() {
     let tokens = lexer(&program);
 }
 
-type Point = (usize, usize);
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct Point(i32, i32);
+
 type FileMap = HashMap<Point, char>;
 
 struct Program {
@@ -38,7 +43,7 @@ fn parse(file: &String) -> FileMap {
         // Iterate over the line
         line.chars().enumerate().for_each(|(j, c)| {
             // Add the character to the hashmap
-            map.insert((i, j), c);
+            map.insert(Point(i as i32, j as i32), c.into());
         });
     });
 
@@ -48,23 +53,95 @@ fn parse(file: &String) -> FileMap {
 /// Lex the file and extract tokens
 fn lexer(program: &Program) {
     // Find the entrypoint
-    let entrypoint_pos = program
+    let entrypoint_pos = *program
         .file
         .iter()
-        .find(|(_, c)| **c == Syntax::symbol(Syntax::Entrypoint))
-        .unwrap().0;
+        .find(|(_, c)| **c == Syntax::symbol(Syntax::Exit))
+        .unwrap()
+        .0;
 
     println!("{:?}", entrypoint_pos);
+
+    // Find every command connected to the entrypoint
+    let mut commands_queue = vec![entrypoint_pos];
+
+    // Track the locations we've already visited
+    let mut visited: HashSet<Point> = HashSet::new();
+    visited.insert(entrypoint_pos);
+
+    while let Some(pos) = commands_queue.pop() {
+        // Get the character at the position
+        let c = program.file.get(&pos).unwrap();
+
+        // Look in the four directions around this position
+        for direction in [(0i32, -1i32), (0, 1), (-1, 0), (1, 0)] {
+            // Get the position in the direction
+            let new_pos = Point(pos.0 + direction.0, pos.1 + direction.1);
+
+            // Get the character at the new position
+            let new_c = program.file.get(&new_pos).unwrap();
+
+            // If the character is a command, add it to the queue
+            match new_c.try_into() {
+                Ok(command) => {
+                    match command {
+                        // If it's a connector, add that to the path
+                        Syntax::VerticalConnector | Syntax::HorizontalConnector => {
+                            if !visited.contains(&new_pos) {
+                                commands_queue.push(new_pos);
+                                visited.insert(new_pos);
+                            }
+                        }
+                        // If we found an entrypoint, print it out
+                        Syntax::Entrypoint => {
+                            if !visited.contains(&new_pos) {
+                                commands_queue.push(new_pos);
+                                visited.insert(new_pos);
+                            }
+                            println!("Found entry at {:?}", new_pos);
+                        }
+                        _ => {}
+                    }
+                }
+                Err(_) => todo!(),
+            }
+        }
+    }
 }
 
 enum Syntax {
     Entrypoint,
+    Exit,
+    VerticalConnector,
+    HorizontalConnector,
+    IntersectingConnector,
+    Floor,
 }
 
 impl Syntax {
     fn symbol(character: Syntax) -> char {
         match character {
             Syntax::Entrypoint => '1',
+            Syntax::Exit => '9',
+            Syntax::VerticalConnector => '|',
+            Syntax::HorizontalConnector => '-',
+            Syntax::IntersectingConnector => '+',
+            Syntax::Floor => '.',
+        }
+    }
+}
+
+/// Convert a character to a syntax
+impl From<&char> for Syntax {
+    fn from(character: &char) -> Syntax {
+        match character {
+            '1' => Syntax::Entrypoint,
+            '9' => Syntax::Exit,
+            '|' => Syntax::VerticalConnector,
+            '-' => Syntax::HorizontalConnector,
+            '.' => Syntax::Floor,
+            '+' => Syntax::IntersectingConnector,
+            _ => panic!("Unknown character"),
         }
     }
 }
